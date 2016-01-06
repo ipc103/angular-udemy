33c5216238582d1442b1da8625644eaded4f4d29
ipc103 <iancandysemail@gmail.com>
2016-01-06 17:39:15 -0500

    finish lesson 3

--- 3/Starter/app.js
+++ 3/Starter/app.js
@@ -1,14 +1,18 @@
 var myApp = angular.module('myApp', []);
 
 myApp.controller('mainController', function($scope) {
-    
-    $scope.name = 'Jane Doe';
-    $scope.occupation = 'Coder';
-    
-    $scope.getname = function() {
-        return 'John Doe';
-    }
-    
-    console.log($scope);
-    
+
+
+
 });
+
+var searchPeople = function (firstname, lastname, height, age, occupation){
+  return 'Jimi Hendrix'
+}
+
+console.log(searchPeople); //you can take any function and turn it into a string
+//angular takes the function you pass to the controller and parses the names of the arguments. Based on the names that you give, it includes different services
+var searchPeopleAnnotated =  angular.injector().annotate(searchPeople);
+console.log(searchPeopleAnnotated);
+// #=> ["firstname", "lastname", "height", "age", "occupation"]
+// Because it's looking for certain names, the arguments can be order independent
