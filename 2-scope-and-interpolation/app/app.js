d5d4bda32e09be1b0243f57f5ba93a7cd0aaa397
ipc103 <iancandysemail@gmail.com>
2016-01-07 10:34:46 -0500

    finish unit 2 lesson 4 show, hide, repeat, class

--- 2-scope-and-interpolation/app/app.js
+++ 2-scope-and-interpolation/app/app.js
@@ -8,10 +8,22 @@ myApp.controller('mainController', ['$scope','$timeout', '$filter',  function($s
     return $filter('lowercase')($scope.handle);
   };
 
+  $scope.characters = 5;
+
+  $scope.players = [
+    {name: 'Antonio Brown'},
+    {name: 'DeAngelo Williams'},
+    {name: 'LeVeon Bell'},
+    {name: 'Martavis Bryant'},
+    {name: 'Cameron Heyward'}
+  ]
+
   setTimeout(function(){
-    $scope.$apply(function(){ // because we're outside context of Angular's, changing the scope's name won't be included as part of the Digest cycle unless we manually apply it to the $scope. This is what $timeout does for you. 
+    $scope.$apply(function(){ // because we're outside context of Angular's, changing the scope's name won't be included as part of the Digest cycle unless we manually apply it to the $scope. This is what $timeout does for you.
       $scope.name = "Jimi";
     })
   }, 3000)
+
+
 
 }]);
