68dd59437e86be334be3b0bd6cb74ec698235582
ipc103 <iancandysemail@gmail.com>
2016-01-07 10:08:15 -0500

    finish unit 2 lesson 3

--- 2-scope-and-interpolation/app/app.js
+++ 2-scope-and-interpolation/app/app.js
@@ -6,5 +6,12 @@ myApp.controller('mainController', ['$scope','$timeout', '$filter',  function($s
   $scope.handle = "";
   $scope.lowercasehandle = function(){
     return $filter('lowercase')($scope.handle);
-  }
+  };
+
+  setTimeout(function(){
+    $scope.$apply(function(){ // because we're outside context of Angular's, changing the scope's name won't be included as part of the Digest cycle unless we manually apply it to the $scope. This is what $timeout does for you. 
+      $scope.name = "Jimi";
+    })
+  }, 3000)
+
 }]);
