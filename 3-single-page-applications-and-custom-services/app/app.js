b8152f74a5152fef138736f8eb7971fcba3fe342
ipc103 <iancandysemail@gmail.com>
2016-01-08 11:56:47 -0500

    finish custom service

--- 3-single-page-applications-and-custom-services/app/app.js
+++ 3-single-page-applications-and-custom-services/app/app.js
@@ -17,16 +17,38 @@ myApp.config(function($routeProvider){ // method that takes a function, inject r
     templateUrl: 'pages/second.html',
     controller: 'secondController'
   })
+});
+
+myApp.service('nameService', function(){
+  this.name = "John Doe";
+  this.namelength = function(){
+    return this.name.length;
+  }
 })
-myApp.controller('mainController', ['$scope', '$location', '$log', function($scope, $location, $log) {
 
-  $scope.name = "Ian - Main Page"
+myApp.controller('mainController', ['$scope', '$location', '$log', 'nameService', function($scope, $location, $log, nameService) {
 
+  $scope.name = nameService.name;
+
+  $scope.$watch('name', function(){ //watches for changes to name - as it's updated, resets the nameService singleton property
+    nameService.name = $scope.name;
+  })
+
+  $log.main = "Property from main!";
+  $log.log(nameService.namelength());
 }]);
 
-myApp.controller('secondController', ['$scope', '$location', '$log', '$routeParams', function($scope, $location, $log, $routeParams) {
+myApp.controller('secondController', ['$scope', '$location', '$log', '$routeParams', 'nameService', function($scope, $location, $log, $routeParams, nameService) {
 
-  $scope.name = "Jimi - Second Page";
+  $scope.name = nameService.name; // scope is a ChildScope - it's different for each controller
+
+  $scope.$watch('name', function(){ //watches for changes to name - as it's updated, resets the nameService singleton property
+    nameService.name = $scope.name;
+  })
+  
   $scope.num = $routeParams.num || "There is no route";
+
+  $log.sec = "Property from second!";
+  $log.log($log); // log is a singleton, it's the same object for each controler
 
 }]);
