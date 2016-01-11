badbc47dd61bd104bdcd0a5ce8827a2c3e671009
ipc103 <iancandysemail@gmail.com>
2016-01-10 19:03:40 -0500

    add ng-submit

--- 5-building-an-app/app/controllers/mainCtrl.js
+++ 5-building-an-app/app/controllers/mainCtrl.js
@@ -1,7 +1,11 @@
-weatherApp.controller('mainCtrl', ['$scope', 'city', function($scope, city){
+weatherApp.controller('mainCtrl', ['$scope','$location', 'city', function($scope, $location, city){
   $scope.name = city.name;
 
   $scope.$watch('name', function(){
     city.name = $scope.name;
   })
+
+  $scope.submit = function(){
+    $location.path('/forecast')
+  }
 }])
